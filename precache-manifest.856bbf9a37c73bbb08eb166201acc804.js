10cc16eae6f499ddf90a8e3026c496f5d63757f1
masha <mariafil2107@gmail.com>
2020-04-10 12:22:33 +0300

    Updates

---
+++ precache-manifest.856bbf9a37c73bbb08eb166201acc804.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dabafb7c951d61d7e1eac1bbd67f064",
+    "url": "./index.html"
+  },
+  {
+    "revision": "177235cccd010fa4ac73",
+    "url": "./static/css/main.6720805e.chunk.css"
+  },
+  {
+    "revision": "0feeb3f4ec8dc8664691",
+    "url": "./static/js/2.ce1d9fc8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.ce1d9fc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "177235cccd010fa4ac73",
+    "url": "./static/js/main.d9c3c4bb.chunk.js"
+  },
+  {
+    "revision": "08fa4d47b8b5db1defa6",
+    "url": "./static/js/runtime-main.1fc83c73.js"
+  },
+  {
+    "revision": "602a6da715d3eddf2dc9a65148241903",
+    "url": "./static/media/OpenSans-Bold.602a6da7.woff"
+  },
+  {
+    "revision": "cbd728bb0047c14298e97591af1e7b9e",
+    "url": "./static/media/OpenSans-Bold.cbd728bb.woff2"
+  },
+  {
+    "revision": "67e0ecdd8281f64dca1f619b3ae0d32e",
+    "url": "./static/media/OpenSans-Regular.67e0ecdd.woff"
+  },
+  {
+    "revision": "9666411f6532dc9c31920ea6059b9728",
+    "url": "./static/media/OpenSans-Regular.9666411f.woff2"
+  },
+  {
+    "revision": "319de3a91f4a87b25af4a815c80f2e20",
+    "url": "./static/media/bg_object.319de3a9.png"
+  },
+  {
+    "revision": "a73352a1b50ee2763170e8dc7d1c077c",
+    "url": "./static/media/pattern.a73352a1.png"
+  }
+]);
